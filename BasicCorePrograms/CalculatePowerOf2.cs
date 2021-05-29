14404abf44e5b55823e40654d0e68fb045ea8dca
pampapathi73 <pampapathik32@gmail.com>
2021-05-29 17:30:22 +0530

    [pampapathi] Added : calculate power of 2

---
+++ BasicCorePrograms/CalculatePowerOf2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    class CalculatePowerOf2
+    {
+        public int num = 0;
+        public void CalcuOfPower()
+        {
+
+            Console.Write("Enter a number:");//input from User
+            int num = Convert.ToInt32(Console.ReadLine());
+            int result = num * 2; //num*2 if 2*2==4
+            Console.WriteLine(result);//Op
+            Console.ReadLine();
+        }
+        }
+}
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -9,6 +9,7 @@ namespace BasicCorePrograms
             Console.WriteLine("Welcome to Basic Core Program");
             Console.WriteLine("1 For: Flip a Coin Head and Tail. ");
             Console.WriteLine("2 For: Select Value is Leap Year Or not");
+            Console.WriteLine("3 For: Select Power of 2 Number");
             int selection = Convert.ToInt32(Console.ReadLine());
 
             // user selection
@@ -24,6 +25,11 @@ namespace BasicCorePrograms
                     //CheckLeapYearOrNot
                     CheckLeapYearOrNot LeapYearOrNot = new CheckLeapYearOrNot();
                     LeapYearOrNot.LeapYear();
+                    break;
+                case 3:
+                    //Power Of 2
+                    CalculatePowerOf2 CalPower = new CalculatePowerOf2();
+                    CalPower.CalcuOfPower();
                     break;
                 default:
                     Console.WriteLine("Invalid Selection. Select in Between 1 to 10");
