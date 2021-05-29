d48ee8f06afec9058a413937672c872f10d5008e
pampapathi73 <pampapathik32@gmail.com>
2021-05-29 17:01:03 +0530

    [pampapathi] Added: leap year or not

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -8,6 +8,7 @@ namespace BasicCorePrograms
         {
             Console.WriteLine("Welcome to Basic Core Program");
             Console.WriteLine("1 For: Flip a Coin Head and Tail. ");
+            Console.WriteLine("2 For: Select Value is Leap Year Or not");
             int selection = Convert.ToInt32(Console.ReadLine());
 
             // user selection
@@ -18,6 +19,11 @@ namespace BasicCorePrograms
                     //FLIP COIN
                     FlipCoinHeadOrTail FlipCoin = new FlipCoinHeadOrTail();
                     FlipCoin.CheckHeadTailPercentage();
+                    break;
+                case 2:
+                    //CheckLeapYearOrNot
+                    CheckLeapYearOrNot LeapYearOrNot = new CheckLeapYearOrNot();
+                    LeapYearOrNot.LeapYear();
                     break;
                 default:
                     Console.WriteLine("Invalid Selection. Select in Between 1 to 10");
