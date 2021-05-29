2e8caebd4bd0dfdbd6d3315fa646587f4aa3d9e1
pampapathi73 <pampapathik32@gmail.com>
2021-05-29 16:43:16 +0530

    [pampapathi] Added: flip coin head or tail

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -7,7 +7,24 @@ namespace BasicCorePrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic Core Program");
-            
+            Console.WriteLine("1 For: Flip a Coin Head and Tail. ");
+            int selection = Convert.ToInt32(Console.ReadLine());
+
+            // user selection
+
+            switch (selection)
+            {
+                case 1:
+                    //FLIP COIN
+                    FlipCoinHeadOrTail FlipCoin = new FlipCoinHeadOrTail();
+                    FlipCoin.CheckHeadTailPercentage();
+                    break;
+                default:
+                    Console.WriteLine("Invalid Selection. Select in Between 1 to 10");
+                    break;
+            }
+            Console.ReadLine();
+
         }
     }
 }
