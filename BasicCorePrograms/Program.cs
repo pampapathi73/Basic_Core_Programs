4657a14ddaee8112d44d4a1ea8edd7dab9672f7a
pampapathi73 <pampapathik32@gmail.com>
2021-05-29 18:06:16 +0530

    [pampapathi] Adde: quotient remainder

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -12,6 +12,7 @@ namespace BasicCorePrograms
             Console.WriteLine("3 For: Select Power of 2 Number");
             Console.WriteLine("4 For: Select the Harmonic Number");
             Console.WriteLine("5 For: Select Factors of number");
+            Console.WriteLine("6 For: Select Quotient and Remainder");
             int selection = Convert.ToInt32(Console.ReadLine());
 
             // user selection
@@ -42,6 +43,11 @@ namespace BasicCorePrograms
                     //Prime Factor
                     PrimeFactorization PrimeFactor = new PrimeFactorization();
                     PrimeFactor.CalcSFactorialSeries();
+                    break;
+                case 6:
+                    //  quotient remainder
+                    QusRemaindercs QuesRem = new QusRemaindercs();
+                    QuesRem.QuotientRemainder();
                     break;
                 default:
                     Console.WriteLine("Invalid Selection. Select in Between 1 to 10");
